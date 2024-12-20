82b3516f4f7e2ee1da9e6e7336bf0815b23188b9
suraj-ta <vummadisetty.sur@tigeranalytics.com>
2024-12-20 09:36:58 +0000

    Updated the nonstandardcode.py file by fixing a small error.

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -19,13 +19,13 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-import pandas as pd
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-housing = load_housing_data
+fetch_housing_data()
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
 
@@ -62,6 +62,8 @@ for set_ in (strat_train_set, strat_test_set):
 housing = strat_train_set.copy()
 housing.plot(kind="scatter", x="longitude", y="latitude")
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+
+housing = housing.drop('ocean_proximity', axis=1)
 
 corr_matrix = housing.corr()
 corr_matrix["median_house_value"].sort_values(ascending=False)
@@ -128,7 +130,9 @@ param_distribs = {
 
 forest_reg = RandomForestRegressor(random_state=42)
 rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distribs,
-                                n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42)
+                                n_iter=10, cv=5, 
+                                scoring='neg_mean_squared_error', 
+                                random_state=42, n_jobs=-1)
 rnd_search.fit(housing_prepared, housing_labels)
 cvres = rnd_search.cv_results_
 for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
