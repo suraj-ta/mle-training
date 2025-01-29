e0cebc0f9905ed30cb23309787fb258da087f2a0
suraj-ta <vummadisetty.sur@tigeranalytics.com>
2025-01-29 17:56:36 +0000

    Built documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/nonstandardcode/"))
+sys.path.insert(0, os.path.abspath("../../src/nonstandardcode/"))
 
 
 project = "nonstandardcode"
